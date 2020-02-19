60cc75c80f0a44e81cae13323daebda63a1a5fe2
skritt3 <25477504+skritt3@users.noreply.github.com>
2020-02-19 23:06:56 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-    client.user.setActivity('Ron Gahbi', {type: 'WATCHING'});
+    client.user.setActivity('Ron Gahbi', {type: 'STREAMING'});
 });
 
 client.on('message', msg => {
     if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
-    if (command === 'guide') return msg.channel.send('https://git.io/d.js-heroku');
+    if (command === 'guide') return msg.channel.send('@MR_Gutsy#3758');
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
 });
 
