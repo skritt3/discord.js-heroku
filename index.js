fea8737eaaaabaaaf0ad35d810416d0c32c5661c
skritt3 <25477504+skritt3@users.noreply.github.com>
2020-02-20 13:56:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -22,13 +22,26 @@ client.on('message', msg => {
     if (command === 'guide') return msg.channel.send('@MR_Gutsy#3758');
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
     } else {
-    const ms = msg.content;
-    if (ms.includes('זדיין')) 
-    {
-    const member = msg.guild.member(msg.author);
-    if(member)
-    member.kick('מת').then(() => { msg.channel.send(`כרגע מת ${msg.author.tag}`); }).catch(err => { msg.channel.send('לצערי אני כבוט ציוני אינני יכול לסלק ילד מת זה'); console.error(err); });
-    }
+        const ms = msg.content;
+        if (ms.includes('זדיין')) 
+        {
+            const member = msg.guild.member(msg.author);
+            if(member)
+            member.kick('מת').then(() => { msg.channel.send(`כרגע מת ${msg.author.tag}`); }).catch(err => { msg.channel.send('לצערי אני כבוט ציוני אינני יכול לסלק ילד מת זה'); console.error(err); });
+        }
+        else if (ms === '/join') {
+            // Only try to join the sender's voice channel if they are in one themselves
+                const channel = client.channels.get("662756581489770530"); 
+                if (!channel) return console.error("The channel does not exist!"); 
+                channel.join()
+                .then(connection => { // Connection is an instance of VoiceConnection
+                  message.reply('I have successfully connected to the channel!');
+                })
+                .catch(console.log);
+                const fs = require('fs');
+                const stream = fs.createReadStream('./Oof.mp3');
+                connection.playStream(stream);
+          }
     }
 });
 
