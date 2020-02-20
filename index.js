c8040f6adff4958e7676fc1668bc4c3c7c53b7de
skritt3 <25477504+skritt3@users.noreply.github.com>
2020-02-20 11:02:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,16 @@ client.on('ready', () => {
 });
 
 client.on('message', msg => {
-    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild) return;
+    if (!msg.content.startsWith(process.env.PREFIX) || !msg.guild)
+    {
     const command = msg.content.split(' ')[0].substr(process.env.PREFIX.length);
     const args = msg.content.split(' ').slice(1).join(' ');
     if (command === 'guide') return msg.channel.send('@MR_Gutsy#3758');
     else if (command === 'invite') return msg.channel.send(process.env.INVITE);
+    } else {
+    const ms = msg.content;
+    if (ms === 'זדיין') msg.author.kick('מת').then(() => { // We let the message author know we were able to kick the person message.reply(`כרגע מת ${user.tag}`); }).catch(err => { // An error happened // This is generally due to the bot not being able to kick the member, // either due to missing permissions or role hierarchy message.reply('לצערי אני כבוט ציוני אינני יכול לסלק ילד מת זה'); // Log the error console.error(err); });
+    }
 });
 
 client.login(process.env.TOKEN);
