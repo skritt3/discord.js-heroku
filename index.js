ad05fcdd621cf78500b89fb3e9a97e9dcf6ae238
skritt3 <25477504+skritt3@users.noreply.github.com>
2020-04-22 21:54:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -39,27 +39,12 @@ client.on('message', msg => {
             } else msg.reply('You need to join a voice channel first!');
           }
         else if (ms === '/leave') {
-            // Only try to join the sender's voice channel if they are in one themselves
-            const member = msg.guild.member(client.user);
-            if(member.voiceChannel)
+           const member = msg.guild.member(client.user);
             member.setVoiceChannel(null)
-                .then(connection => { // Conn// ection is an instance of VoiceConnection
-                    msg.reply('I have successfully left the channel!');
+                .then(connection => { 
+                    msg.reply('I have successfully left the channel!');	                  
                 })
                 .catch(console.log);
-            else if (msg.member.voiceChannel) {
-                msg.member.voiceChannel.join()
-                    .then(connection => { // Conn// ection is an instance of VoiceConnection
-                        msg.reply('שמע טוב יבן שרמוטה ישרליל יהומו ידוחף בולבולים לתחת יבולעע ימידיין בתחת יבן בן זונה כוסלאלאמשלך הזונה הזאת הבת זונה הזאת של היום חברים שלך עומדים מיתחת לחלון ואמשלך מיתיצבת בשעה 7 וצועקים אנחנו רוצים לזיין יבן זונה גומרים לה מהחלון והיא תופסת מילמטהאמאמשלך כל כך זונה מיסכנה רק עוזרים לה היא חרמנית מתה אפילו בתור סטוץ לא היו לוקחים אותה מרוב שהכוס שלה פתוח כבר אפשר להכניס תראש שלך ימידייןןן יבן אלף זונות אבא שלך חשפן יבן זונה יטמבל אבא שלך כל חמישי מקבל צינור בתחת אין פלא שהוא אוהב לשבת על אופנים בלי מושב יא כושילאמאמאמאמא שלך ימדיין ימקבלכל יום בתחת מסודני יזונה מה ניראלךךךך למי אתה חושב שאתה מדבר יהומו משתיינים על כמוך ועל אמא שלך בקשת יבן זונה יבן בן זונה יבן אלףףףף עשו אותך מסטוץ של דנה אינטרנשנל בתוך מונית שרות ימוצצ ישרמוטה , יבתול כייף בראבק יטמבל ימיבכן יבןבןבןבןבןבןבן זונה , טפייייי עליך יבן שרלילות יהומו יכלב יבן אלףףף , אמא שלך על אח שלך מצלם יא זיין ביצה שמאלית שלי אתה יבן שרמוטה יהומו מיזדדיין אמא שלך עשתה אותך ביחד עם אבא שלך על שולחן סונקר ימיזיין בתחת יבן אלף שרלילות ישלך ראש של כדור יבן זונה אתה ניכנס ישר לחור של אמא שלך הזונה הזאת כולם היום הרגלים שלה פתוחות אפשר להכניס שם בית יטמבל מרוב שהיא זונה יבן אלף זונות ביגלל ילדים כמוך יש מילחמות בארץ');
-                        const stream = fs.createReadStream('./Oof.mp3');
-                        const voice = connection.playStream(stream);
-                        voice.on("end", () => {
-                            connection.channel.leave();
-                        })
-                    })
-                    .catch(console.log);
-            } else msg.reply('You need to join a voice channel first!');
-        }
     }
 });
 
