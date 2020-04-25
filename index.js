ead938e75f653ef40c1bd0433cf1dd843985c723
skritt3 <25477504+skritt3@users.noreply.github.com>
2020-04-25 13:41:57 +0300

    Update index.js

--- index.js
+++ index.js
@@ -47,8 +47,9 @@ client.on('message', msg => {
                 .catch(console.log);
         }
        else if (ms === '/nigg') {
-           for (let i = 0; i < 100; i++) {
-               msg.reply('Nigger');
+           for (let i = 0; i < 50; i++) {
+               const sentMessage = await msg.reply('Nigger');
+               await sentMessage.delete({ timeout: 1000 });
             }
         }
     }
