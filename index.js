41301287dde6a99ea2767761148f3e594b74a6a5
skritt3 <25477504+skritt3@users.noreply.github.com>
2020-04-27 21:43:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,20 @@ client.on('message', msg => {
                     .catch(console.log);
             } else msg.reply('You need to join a voice channel first!');
         }
+        else if (ms === '/bruh') {
+            // Only try to join the sender's voice channel if they are in one themselves
+            if (msg.member.voiceChannel) {
+                msg.member.voiceChannel.join()
+                    .then(connection => { // Conn// ection is an instance of VoiceConnection
+                        const stream = fs.createReadStream('./Bruh.mp3');
+                        const voice = connection.playStream(stream);
+                        voice.on("end", () => {
+                            connection.channel.leave();
+                        })
+                    })
+                    .catch(console.log);
+            } else msg.reply('You need to join a voice channel first!');
+        }
         else if (ms === '/leave') {
             const member = msg.guild.member(client.user);
             member.setVoiceChannel(null)
