6cb88d80528f2b9af5d005af2eb4f59f2c373a8a
skrittdev <25477504+skrittdev@users.noreply.github.com>
2020-08-19 14:56:31 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 const fs = require('fs');
 
 client.on('ready', () => {
-    client.user.setActivity('with my balls', {type: 'STREAMING',url: 'https://www.twitch.tv/xdinem'});
+    client.user.setActivity('my balls', {type: 'STREAMING',url: 'https://www.twitch.tv/xdinem'});
 });
 
 client.on('message', msg => {
