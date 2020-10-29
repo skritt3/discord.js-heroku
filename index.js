c65f8082752d46477b1688d4887096deb025075c
skrittdev <25477504+skrittdev@users.noreply.github.com>
2020-10-29 11:17:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -319,9 +319,7 @@ client.on('message', msg => {
         else if(msg.channel.id == '715191707263565864' && ms.includes('lofi'))
         {
             client.setTimeout(function(){
-                console.log(JSON.stringify(msg.channel.members, null, 4));
-                /*;
-                msg.guild.members.get('235088799074484224').setVoiceChannel(null);*/
+                msg.guild.fetchMember('235088799074484224').then(console.log);
             },1000);
         }
         else if ((ms.includes('nig') || ms.includes('Ниг') || ms.includes('ניג') || ms.includes('chicken')
