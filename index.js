15a001e789ff5a9148243ed97f79cae51be27c2b
dinem-debug <69425958+dinem-debug@users.noreply.github.com>
2021-01-26 15:31:25 +0200

    Update index.js

--- index.js
+++ index.js
@@ -80,6 +80,20 @@ client.on('message', msg => {
                     .catch(console.log);
             } else msg.reply('You need to join a voice channel first!');
         }
+         else if (ms === '/gang') {
+            // Only try to join the sender's voice channel if they are in one themselves
+            if (msg.member.voice.channel) {
+                msg.member.voice.channel.join()
+                    .then(connection => { // Conn// ection is an instance of VoiceConnection
+                        msg.reply('I have successfully connected to the channel!');
+                        const voice = connection.play('./ganga.mp3');
+                        voice.on('finish', () => {
+                            connection.channel.leave();
+                        })
+                    })
+                    .catch(console.log);
+            } else msg.reply('You need to join a voice channel first!');
+        }
         else if (ms === '/butterdog') {
             // Only try to join the sender's voice channel if they are in one themselves
             if (msg.member.voice.channel) {
