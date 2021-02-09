2938e7b38ce0766802c2af3918fbe94133d613ee
dinem-debug <69425958+dinem-debug@users.noreply.github.com>
2021-02-09 14:59:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ client.on('ready', () => {
 client.on('message', msg => {
     if(!msg.guild) return;
     const ms = msg.content.toLowerCase();
-    if ((ms.includes('nigger') || ms.includes('Ниг') || ms.includes('ניג') 
+    if ((ms.includes('nigger') || ms.includes('Ниг') || ms.includes('ניג') || ms.includes('nigga')|| ms.includes('nigge')|| ms.includes('nigg')
             || ms.includes('nugget')  || ms.includes('נאגט')
             || ms.includes('נייגר')|| ms.includes('נגר')|| ms.includes('lo fi')|| ms.includes('אמאשך')|| ms.includes('אמא שך')|| ms.includes('אבא שך')|| ms.includes('אבא שלך')|| ms.includes('אמא שלך')|| ms.includes('lo fi')|| ms.includes('אבאשך')|| ms.includes('toxic')|| ms.includes('טוקסיק')|| ms.includes('ni gg')|| ms.includes('nagar')|| ms.includes('naygar')|| ms.includes('naagar'))
             && msg.channel.id !== '703603549191405638' && !msg.member.roles.cache.find(r => r.id === "734842442834247782")) {
