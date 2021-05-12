4055f99ba39e85a068b55f7af1ae9daa439a4c66
skrittdev <25477504+skrittdev@users.noreply.github.com>
2021-05-12 21:14:45 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,14 +16,15 @@ client.on('ready', () => {
             type: "PLAYING" //PLAYING: WATCHING: LISTENING: STREAMING:
         }
     });*/
-   var url = "https://www.tzevaadom.co.il/historyjson.php";
+    var url = "https://www.tzevaadom.co.il/historyjson.php";
     request({
         url: url,
         json: true
     }, function (error, response, body) {
 
         if (!error && response.statusCode === 200) {
-            ld=new Date(body[0]['date']);
+            ld=new Date(((body[0]['date'])*1000)+10800000);
+            console.log(ld);
         }
     });
 });
@@ -32,10 +33,10 @@ client.on('message', msg => {
     if(!msg.guild) return;
     const ms = msg.content.toLowerCase();
     if ((ms.includes('nigger') || ms.includes('Ниг') || ms.includes('ניג') || ms.includes('nigga')|| ms.includes('nigge')|| ms.includes('nigg')
-            || ms.includes('nugget')  || ms.includes('נאגט')
-            || ms.includes('נייגר')|| ms.includes('נגר')|| ms.includes('אמאשך')|| ms.includes('אמא שך')|| ms.includes('אבא שך')|| ms.includes('אבא שלך')|| ms.includes('אמא שלך')|| ms.includes('lo fi')|| ms.includes('אבאשך')|| ms.includes('toxic')|| ms.includes('טוקסיק')|| ms.includes('ni gg')|| ms.includes('nagar')|| ms.includes('naygar')|| ms.includes('naagar'))
-            && msg.channel.id !== '703603549191405638' && !msg.member.roles.cache.find(r => r.id === "734842442834247782")) {
-            msg.delete();
+        || ms.includes('nugget')  || ms.includes('נאגט')
+        || ms.includes('נייגר')|| ms.includes('נגר')|| ms.includes('אמאשך')|| ms.includes('אמא שך')|| ms.includes('אבא שך')|| ms.includes('אבא שלך')|| ms.includes('אמא שלך')|| ms.includes('lo fi')|| ms.includes('אבאשך')|| ms.includes('toxic')|| ms.includes('טוקסיק')|| ms.includes('ni gg')|| ms.includes('nagar')|| ms.includes('naygar')|| ms.includes('naagar'))
+        && msg.channel.id !== '703603549191405638' && !msg.member.roles.cache.find(r => r.id === "734842442834247782")) {
+        msg.delete();
     }
     else if(msg.channel.id !== '779780376498798632' && msg.channel.id !== '841996869890080779') return;
     else if (msg.content.startsWith(process.env.PREFIX))
@@ -136,7 +137,7 @@ client.on('message', msg => {
                     .catch(console.log);
             } else msg.reply('You need to join a voice channel first!');
         }
-         else if (ms === '/gang') {
+        else if (ms === '/gang') {
             // Only try to join the sender's voice channel if they are in one themselves
             if (msg.member.voice.channel) {
                 msg.member.voice.channel.join()
@@ -277,8 +278,8 @@ client.on('message', msg => {
             } else msg.reply('You need to join a voice channel first!');
         }
         else if (ms === '/ra') {
-           rc=msg.channel;
-           msg.reply('Red Alert channel set!');
+            rc=msg.channel;
+            msg.reply('Red Alert channel set!');
         }
         else if (ms === '/leave') {
             const member = msg.guild.member(client.user);
@@ -331,22 +332,22 @@ client.on('message', msg => {
                 msg.reply('No user selected');
             }
         }
-            /*else if (ms.startsWith('/uncum')) {
-                const user = msg.mentions.users.first();
-                if(user)
+        /*else if (ms.startsWith('/uncum')) {
+            const user = msg.mentions.users.first();
+            if(user)
+            {
+                if(msg.member.roles.some(r=>["Admins"].includes(r.name)) )
                 {
-                    if(msg.member.roles.some(r=>["Admins"].includes(r.name)) )
-                    {
-                        const mm = msg.guild.member(user);
-                        mm.setNickname(mm.user.username);
-                        msg.reply('Uncum applied on <@'+mm.user.id+'>');
-                    } else msg.reply('No permissions');
+                    const mm = msg.guild.member(user);
+                    mm.setNickname(mm.user.username);
+                    msg.reply('Uncum applied on <@'+mm.user.id+'>');
+                } else msg.reply('No permissions');
 
-                } else {
-                    msg.member.setNickname(msg.member.user.username);
-                    msg.reply('Uncum applied');
-                }
-            }*/
+            } else {
+                msg.member.setNickname(msg.member.user.username);
+                msg.reply('Uncum applied');
+            }
+        }*/
         /*else if(msg.channel.id == '715191707263565864' && (ms.includes('lofi') ))
         {
             client.setTimeout(function(){
@@ -368,25 +369,26 @@ var task=cron.schedule('*/2 * * * * *', () => {
         json: true
     }, function (error, response, body) {
         //console.log(response.statusCode+' "'+body);
-        if (!error && response.statusCode === 200) {
-            for (var i=body.length; i>=0; i++)
+        if (!error && response.statusCode === 200 && ld!=null) {
+            for (var i=body.length-1; i>=0; i--)
             {
-                var dt=new Date(body[i]['date']);
-                console.log(body.length+1-i+': '+dt.toISOString().replace(/T/, ' ').replace(/\..+/, '')+' '+body[i]['area']);
-                if(dt > ld && body[i]['area'].indexOf('אשדוד') !=-1)
-                {
-                    ld=dt;
-                    const exampleEmbed = new Discord.MessageEmbed()
-                        .setColor('#ff0000')
-                        .setTitle(body[i]['title'])
-                        .setDescription('<@842008317220225054>')
-                        .setThumbnail('https://www.oref.org.il/Images/Logo_pakar.png')
-                        .addFields(
-                            { name: 'מיקום', value: body[i]['area'] },
-                            { name: 'תאריך ןשעה', value: dt.toISOString().replace(/T/, ' ').replace(/\..+/, '') },
-                        )
-                        .setTimestamp();
-                    rc.send(exampleEmbed);
+                if(body[i]!==undefined) {
+                    var dt = new Date(((body[i]['date'])*1000)+10800000);
+                    console.log(body.length - i + ': ' + dt.toISOString().replace(/T/, ' ').replace(/\..+/, '') + ' ' + body[i]['area']);
+                    if (dt > ld && body[i]['area'].indexOf('אשדוד') != -1) {
+                        ld = dt;
+                        const exampleEmbed = new Discord.MessageEmbed()
+                            .setColor('#ff0000')
+                            .setTitle(body[i]['title'])
+                            .setDescription('<@842008317220225054>')
+                            .setThumbnail('https://www.oref.org.il/Images/Logo_pakar.png')
+                            .addFields(
+                                {name: 'מיקום', value: body[i]['area']},
+                                {name: 'תאריך ןשעה', value: dt.toISOString().replace(/T/, ' ').replace(/\..+/, '')},
+                            )
+                            .setTimestamp();
+                        rc.send(exampleEmbed);
+                    }
                 }
             }
         }
